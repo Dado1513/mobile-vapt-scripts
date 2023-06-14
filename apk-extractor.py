c4d5807b3279c7cae606eb10d4b5fc949a0abf58
Davide Caputo <dave.caputo93@gmail.com>
2023-06-14 17:07:22 +0200

    add apk-extractor

---
+++ apk-extractor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import sys
+import rich
+from loguru import logger
+import subprocess
+
+
+if __name__=="__main__":
+    if len(sys.argv) > 2:
+        pacakge_name = sys.argv[1]
+        output_dir = sys.argv[2]
+        os.makedirs(output_dir, exist_ok=True)
+        output = subprocess.check_output(["adb", "shell", "pm","path", pacakge_name]).decode('utf-8').strip()
+        list_apk = output.split("\n")
+        for apk in list_apk:
+            path = apk.split("package:")[1]
+            output_downloaded_app = subprocess.call(["adb","pull",path, output_dir], stdout=os.devnull, stderr=os.devnull)
+            logger.info(f"Downloading app {apk}")
+    else:
+        logger.error("Error. Usage:")
+        logger.error("./apk-extractor.py <package_name> <output_dir>")
+    
