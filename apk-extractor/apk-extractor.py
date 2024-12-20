08b7b723fb7916e97309b8cddcf90b6bbf3e9160
Davide Caputo <dave.caputo93@gmail.com>
2024-12-20 15:11:53 +0100

    Update python script for windows

--- apk-extractor/apk-extractor.py
+++ apk-extractor/apk-extractor.py
@@ -4,20 +4,24 @@ import sys
 import rich
 from loguru import logger
 import subprocess
+from pathlib import Path
+
+
 
 
 if __name__=="__main__":
     if len(sys.argv) > 2:
-        pacakge_name = sys.argv[2]
-        output_dir = sys.argv[1]
+        package_name = sys.argv[2]
+        output_dir = Path(sys.argv[1])
         os.makedirs(output_dir, exist_ok=True)
-        output = subprocess.check_output(["adb", "shell", "pm","path", pacakge_name]).decode('utf-8').strip()
+        output = subprocess.check_output(["adb", "shell", "pm","path", package_name]).decode('utf-8').strip()
         list_apk = output.split("\n")
         for apk in list_apk:
             path = apk.split("package:")[1]
-            output_downloaded_app = subprocess.call(["adb","pull",path, output_dir], stdout=os.devnull, stderr=os.devnull)
+            output_downloaded_app = subprocess.call(["adb","pull", path, output_dir])
             logger.info(f"Downloading app {apk}")
     else:
         logger.error("Error. Usage:")
-        logger.error("./apk-extractor.py <package_name> <output_dir>")
+        logger.error("./apk-extractor.py  <output_dir> <package_name>")
     
+
